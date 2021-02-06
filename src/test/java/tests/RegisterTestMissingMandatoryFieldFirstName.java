45c9776f576fa573fd99be533b7861bdf75a0faf
bimpi <bimpi@wp.pl>
2021-02-06 16:00:22 +0100

    login test

--- src/test/java/tests/RegisterTestMissingMandatoryFieldFirstName.java
+++ src/test/java/tests/RegisterTestMissingMandatoryFieldFirstName.java
@@ -2,16 +2,15 @@ package tests;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
-import org.openqa.selenium.By;
 import pageobjects.HomePage;
 import pageobjects.LoginPage;
 import pageobjects.RegisterPage;
 import utils.RandomUser;
 
-public class RegisterTest extends BaseTest {
+public class RegisterTestMissingMandatoryFieldFirstName extends BaseTest {
 
     @Test
-    void shouldRegisterUserWhenMandatoryFieldsAreFilled() {
+    void shouldDisplayCorrectAlertsWhenMandatoryDataIsMissing() {
         HomePage homePage = new HomePage(driver);
         homePage.openPage();
         homePage.goToLoginPage();
@@ -21,12 +20,9 @@ public class RegisterTest extends BaseTest {
         loginPage.goToRegisterPage(randomUser.email);
 
         RegisterPage registerPage = new RegisterPage(driver);
+        randomUser.firstName = "";
         registerPage.registerUser(randomUser);
-        Assertions.assertTrue(homePage.isUserLoggedIn());
 
-        // jakaś asercja/asercje
-        Assertions.assertEquals("Sign out", driver.findElement(By.className("logout")).getText());
-        Assertions.assertEquals(randomUser.firstName + " " + randomUser.lastName,
-                driver.findElement(By.xpath("//a[@class=\"account\"]/span")).getText());
+        Assertions.assertTrue(registerPage.isAlertDisplayed("firstname is required."));
     }
 }
