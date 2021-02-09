7bb2ac1b679324ef7e4b1555d7c5c23dec40e5ee
bimpi <bimpi@wp.pl>
2021-02-09 22:42:52 +0100

    new Register Tests

--- src/test/java/tests/RegisterTest.java
+++ src/test/java/tests/RegisterTest.java
@@ -2,7 +2,6 @@ package tests;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
-import org.openqa.selenium.By;
 import pageobjects.HomePage;
 import pageobjects.LoginPage;
 import pageobjects.RegisterPage;
@@ -24,5 +23,53 @@ public class RegisterTest extends BaseTest {
 
         Assertions.assertTrue(homePage.isUserLoggedIn(randomUser.firstName, randomUser.lastName));
         Assertions.assertTrue(homePage.isUserLoggedIn());
+    }
+
+    @Test
+    void shouldDisplayCorrectAlertsWhenPasswordIsMissing() {
+        HomePage homePage = new HomePage(driver);
+        homePage.openPage();
+
+        LoginPage loginPage = homePage.goToLoginPage();
+        RandomUser randomUser = new RandomUser();
+        loginPage.goToRegisterPage(randomUser.email);
+
+        RegisterPage registerPage = new RegisterPage(driver);
+        randomUser.password = "";
+        registerPage.registerUser(randomUser);
+
+        Assertions.assertTrue(registerPage.isAlertDisplayed("passwd is required."));
+    }
+
+    @Test
+    void shouldDisplayCorrectAlertsWhenFirstNameIsMissing() {
+        HomePage homePage = new HomePage(driver);
+        homePage.openPage();
+
+        LoginPage loginPage = homePage.goToLoginPage();
+        RandomUser randomUser = new RandomUser();
+        loginPage.goToRegisterPage(randomUser.email);
+
+        RegisterPage registerPage = new RegisterPage(driver);
+        randomUser.firstName = "";
+        registerPage.registerUser(randomUser);
+
+        Assertions.assertTrue(registerPage.isAlertDisplayed("firstname is required."));
+    }
+
+    @Test
+    void shouldDisplayCorrectAlertsWhenLastNameIsMissing() {
+        HomePage homePage = new HomePage(driver);
+        homePage.openPage();
+
+        LoginPage loginPage =  homePage.goToLoginPage();
+        RandomUser randomUser = new RandomUser();
+        loginPage.goToRegisterPage(randomUser.email);
+
+        RegisterPage registerPage = new RegisterPage(driver);
+        randomUser.lastName = "";
+        registerPage.registerUser(randomUser);
+
+        Assertions.assertTrue(registerPage.isAlertDisplayed("lastname is required."));
     }
 }
